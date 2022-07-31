29e24e3e4a8c25ee97e8d68b94824b033c053ce4
henricker <henriquevieira@alu.ufc.br>
2022-07-31 11:01:06 -0300

    test: should call imap to listening 'end' event

--- __tests__/infra/services/mail-listener.spec.ts
+++ __tests__/infra/services/mail-listener.spec.ts
@@ -4,9 +4,15 @@ import { imap } from '../../../src/infra/config/imap';
 const mailListenerService = new MailListenerService(imap);
 
 describe('Mail Listener service', () => {
-  it('Shoud call imap to listening error', () => {
+  it('Shoud call imap to listening error event', () => {
     const onSpy = jest.spyOn(imap, 'on');
     mailListenerService['handleOnError']();
     expect(onSpy).toHaveBeenCalledWith('error', expect.any(Function));
+  });
+
+  it('Should call imap to listening end event', () => {
+    const onSpy = jest.spyOn(imap, 'on');
+    mailListenerService['handleOnEnd']();
+    expect(onSpy).toHaveBeenCalledWith('end', expect.any(Function));
   });
 });
