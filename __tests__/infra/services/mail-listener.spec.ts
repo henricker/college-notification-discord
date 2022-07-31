bbf137571e031de30b663e0286a5c57215041b1b
henricker <henriquevieira@alu.ufc.br>
2022-07-31 10:59:46 -0300

    style: run lint

--- __tests__/infra/services/mail-listener.spec.ts
+++ __tests__/infra/services/mail-listener.spec.ts
@@ -1,12 +1,12 @@
-import { MailListenerService } from "../../../src/infra/services/mail-listener.service";
+import { MailListenerService } from '../../../src/infra/services/mail-listener.service';
 import { imap } from '../../../src/infra/config/imap';
 
 const mailListenerService = new MailListenerService(imap);
 
 describe('Mail Listener service', () => {
-    it('Shoud call imap to listening error', () => {
-        const onSpy = jest.spyOn(imap, 'on');
-        mailListenerService['handleOnError']();
-        expect(onSpy).toHaveBeenCalledWith('error', expect.any(Function));
-    })
-})
+  it('Shoud call imap to listening error', () => {
+    const onSpy = jest.spyOn(imap, 'on');
+    mailListenerService['handleOnError']();
+    expect(onSpy).toHaveBeenCalledWith('error', expect.any(Function));
+  });
+});
--- src/infra/services/mail-listener.service.ts
+++ src/infra/services/mail-listener.service.ts
@@ -1,17 +1,17 @@
 import Imap from 'imap';
 
 export class MailListenerService {
-    constructor(private readonly imap: Imap) {}
+  constructor(private readonly imap: Imap) {}
 
-    private handleOnError() {
-        this.imap.on('error', (error: Error) => {
-            console.error(error);
-        });
-    }
+  private handleOnError() {
+    this.imap.on('error', (error: Error) => {
+      console.error(error);
+    });
+  }
 
-    private handleOnEnd() {
-        this.imap.on('end', () => {
-            console.log('Connection ended');
-        });
-    }
+  private handleOnEnd() {
+    this.imap.on('end', () => {
+      console.log('Connection ended');
+    });
+  }
 }
