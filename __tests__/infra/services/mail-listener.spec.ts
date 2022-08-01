f8660941281c022e9f9e735c792eaa22ec653b6d
henricker <henriquevieira@alu.ufc.br>
2022-08-01 14:46:16 -0300

    test: add unit tests to connect method of fetch mail service

--- __tests__/infra/services/mail-listener.spec.ts
+++ __tests__/infra/services/mail-listener.spec.ts
@@ -1,18 +1,47 @@
-import { MailListenerService } from '../../../src/infra/services/mail-listener.service';
-import { imap } from '../../../src/infra/config/imap';
+import { FetchMailService } from '../../../src/infra/services/mail-listener.service';
 
-const mailListenerService = new MailListenerService(imap);
+const imapConfigMock = {
+  imap: {
+    user: 'test',
+    password: 'test',
+    host: 'test'
+  }
+};
+const fetchMailService = new FetchMailService(imapConfigMock);
 
-describe('Mail Listener service', () => {
-  it('Shoud call imap to listening error event', () => {
-    const onSpy = jest.spyOn(imap, 'on');
-    mailListenerService['handleOnError']();
-    expect(onSpy).toHaveBeenCalledWith('error', expect.any(Function));
-  });
+jest.mock('imap-simple', () => {
+  return {
+    connect: jest.fn((config) => {
+      return {
+        openBox: jest.fn((boxName, cb) => {}),
+        addFlags: jest.fn((id, flags, cb) => {}),
+        search: jest.fn((searchCriteria, fetchOptions, cb) => {})
+      };
+    })
+  };
+});
 
-  it('Should call imap to listening end event', () => {
-    const onSpy = jest.spyOn(imap, 'on');
-    mailListenerService['handleOnEnd']();
-    expect(onSpy).toHaveBeenCalledWith('end', expect.any(Function));
+describe('# Fetch Mail (service)', () => {
+  describe('connect (method)', () => {
+    it('Should call connect method by imap with correct values when connect method of fetch mail has called', () => {
+      const connectSpy = jest.spyOn(fetchMailService['imap'], 'connect');
+      fetchMailService.connect();
+      expect(connectSpy).toHaveBeenCalledWith(imapConfigMock);
+    });
+
+    it('Should throw if connect method of imap throws', () => {
+      const connectSpy = jest.spyOn(fetchMailService['imap'], 'connect');
+      connectSpy.mockImplementationOnce(() => {
+        throw new Error('Error');
+      });
+      fetchMailService.connect().catch((err) => {
+        expect(err).toBeInstanceOf(Error);
+      });
+    });
+
+    it('Should return itself instance on success connection', () => {
+      jest.spyOn(fetchMailService['imap'], 'connect');
+      expect(fetchMailService.connect()).resolves.toBe(fetchMailService);
+    });
   });
 });
