c8e46af709d21c186e0704f4c19f52c01e1a6fec
henricker <henriquevieira@alu.ufc.br>
2022-08-01 16:10:53 -0300

    tests: add tests to openBox and handleOpenBox methods of fetchMail service

--- __tests__/infra/services/mail-listener.spec.ts
+++ __tests__/infra/services/mail-listener.spec.ts
@@ -7,7 +7,6 @@ const imapConfigMock = {
     host: 'test'
   }
 };
-const fetchMailService = new FetchMailService(imapConfigMock);
 
 jest.mock('imap-simple', () => {
   return {
@@ -24,12 +23,14 @@ jest.mock('imap-simple', () => {
 describe('# Fetch Mail (service)', () => {
   describe('connect (method)', () => {
     it('Should call connect method by imap with correct values when connect method of fetch mail has called', () => {
+      const fetchMailService = new FetchMailService(imapConfigMock);
       const connectSpy = jest.spyOn(fetchMailService['imap'], 'connect');
       fetchMailService.connect();
       expect(connectSpy).toHaveBeenCalledWith(imapConfigMock);
     });
 
     it('Should throw if connect method of imap throws', () => {
+      const fetchMailService = new FetchMailService(imapConfigMock);
       const connectSpy = jest.spyOn(fetchMailService['imap'], 'connect');
       connectSpy.mockImplementationOnce(() => {
         throw new Error('Error');
@@ -40,8 +41,74 @@ describe('# Fetch Mail (service)', () => {
     });
 
     it('Should return itself instance on success connection', () => {
+      const fetchMailService = new FetchMailService(imapConfigMock);
       jest.spyOn(fetchMailService['imap'], 'connect');
       expect(fetchMailService.connect()).resolves.toBe(fetchMailService);
+    });
+  });
+
+  describe('openBox (method)', () => {
+    it('Should throw if connection with imap not stablished', () => {
+      const fetchMailService = new FetchMailService(imapConfigMock);
+      fetchMailService['connectionImap'] = null;
+      expect(fetchMailService.openBox('INBOX')).rejects.toThrow();
+    });
+
+    it('Should call openBox method of imap with correct values when openBox method of fetch mail has called', async () => {
+      const fetchMailService = new FetchMailService(imapConfigMock);
+      await fetchMailService.connect();
+
+      const openBoxSpy = jest.spyOn(
+        fetchMailService['connectionImap'] as any,
+        'openBox'
+      );
+      fetchMailService.openBox('INBOX');
+
+      expect(openBoxSpy.mock.calls[0][0]).toBe('INBOX');
+      expect((openBoxSpy.mock.calls[0][1] as Function).name).toBe(
+        fetchMailService['handleOpenBox'].bind(fetchMailService).name
+      );
+    });
+  });
+
+  describe('handleOpenBox (method)', () => {
+    it('Should throw if error has been passed', () => {
+      const fetchMailService = new FetchMailService(imapConfigMock);
+      expect(
+        fetchMailService['handleOpenBox'](new Error('Error'), 'INBOX')
+      ).rejects.toThrow();
+    });
+
+    it('Should call search method of imap with correct values if all right', async () => {
+      const fetchMailservice = new FetchMailService(imapConfigMock);
+      await fetchMailservice.connect();
+
+      const searchSpy = jest.spyOn(
+        fetchMailservice['connectionImap'] as any,
+        'search'
+      );
+      jest.useFakeTimers().setSystemTime(new Date('2022-04-17'));
+      const delay = 24 * 3600 * 1000;
+      const yesterday = new Date();
+      yesterday.setTime(Date.now() - delay);
+
+      const testCriteria = ['UNSEEN', ['SINCE', yesterday]];
+      const testFetchOpttions = {
+        bodies: ['HEADER', 'TEXT', '']
+      };
+
+      fetchMailservice['handleOpenBox'](null, 'INBOX');
+
+      const firstArgument = searchSpy.mock.calls[0][0];
+      const secondArgument = searchSpy.mock.calls[0][1];
+      const thirdArgument = (searchSpy.mock.calls[0][2] as Function).name;
+
+      expect(firstArgument).toEqual(testCriteria);
+      expect(secondArgument).toEqual(testFetchOpttions);
+      expect(thirdArgument).toBe(
+        fetchMailservice['handleSearchBox'].bind(fetchMailservice).name
+      );
+      jest.useRealTimers();
     });
   });
 });
