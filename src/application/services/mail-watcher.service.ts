a2b5ddcfe5d3fdc7c9736e94a786e81bfb5bc485
henricker <henriquevieira@alu.ufc.br>
2022-08-02 15:51:28 -0300

    feat: add mailer watcher

--- src/application/services/mail-watcher.service.ts
+++ src/application/services/mail-watcher.service.ts
@@ -1,5 +1,36 @@
-import { FetchMailService } from '../../infra/services/fetch-mail.service';
+import {
+  FetchMailService,
+  MailType
+} from '../../infra/services/fetch-mail.service';
 
 export class MailWatcherService {
-  constructor(private readonly fetchMailService: FetchMailService) {}
+  constructor(private readonly fetchMailService: FetchMailService) {
+    this.fetchMailService.on(
+      'finish-read-messages',
+      this.handleOnFinishReadMailsFounded.bind(this)
+    );
+
+    this.fetchMailService.on('nothing-email-founded', async () => {
+      console.info('Nothing mails found');
+      await this.fetchMailService.disconnect();
+    });
+  }
+  async watch(interval: number) {
+    console.info('Initializing mail watcher');
+    setInterval(this.handleWatchMails.bind(this), interval);
+  }
+
+  private async handleOnFinishReadMailsFounded(mails: MailType[]) {
+    console.info('New mails found');
+    console.log(mails);
+    await this.fetchMailService.disconnect();
+  }
+
+  private async handleWatchMails() {
+    try {
+      (await this.fetchMailService.connect())?.openBox('INBOX');
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
